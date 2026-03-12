3b05a9dec626afa503afcc6d593316337eede079
gxnca <fl11gg20@gmail.com>
2026-03-12 19:50:14 +0000

    fix: lint

--- lib/gallium_web.ex
+++ lib/gallium_web.ex
@@ -89,8 +89,8 @@ defmodule GalliumWeb do
       import GalliumWeb.CoreComponents
 
       # Common modules used in templates
-      alias Phoenix.LiveView.JS
       alias GalliumWeb.Layouts
+      alias Phoenix.LiveView.JS
 
       # Routes generation with the ~p sigil
       unquote(verified_routes())
