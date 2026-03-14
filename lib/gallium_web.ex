deb33e2a8dfffc7d8889f8558a277820ffc1975f
Gonçalo Fernandes <149530330+gxnca@users.noreply.github.com>
2026-03-14 19:38:13 +0000

    fix: frames' names (#26)

--- lib/gallium_web.ex
+++ lib/gallium_web.ex
@@ -17,7 +17,7 @@ defmodule GalliumWeb do
   those modules here.
   """
 
-  def static_paths, do: ~w(assets fonts images favicon.ico robots.txt)
+  def static_paths, do: ~w(assets fonts images frames favicon.ico robots.txt)
 
   def router do
     quote do
