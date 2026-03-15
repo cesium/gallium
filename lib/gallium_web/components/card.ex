7680d1bc2161aae265c9238c0e8c387d9623a238
deboravcaetano <deboravfx@gmail.com>
2026-03-15 10:14:53 +0000

    fix: review

--- lib/gallium_web/components/card.ex
+++ lib/gallium_web/components/card.ex
@@ -6,18 +6,28 @@ defmodule GalliumWeb.Card do
 
   attr :title, :string, default: ""
   attr :description, :string, default: ""
+  attr :image_url, :string, default: nil
   attr :icon, :string, default: nil
 
   def card(assigns) do
     ~H"""
-    <div class="rounded-lg flex flex-col p-8 items-center gap-6 w-full max-w-[280px] min-h-[288px] h-fit bg-beige">
-      <div :if={@icon} class="flex w-16 h-16 items-center justify-center rounded-full bg-bronze-100">
-        <.icon name={@icon} class="h-8 w-8 text-bronze-500" />
+    <div class="rounded-lg flex flex-col p-8 items-center gap-6 w-full max-w-xs min-h-72 h-fit bg-beige">
+      <div
+        :if={@image_url || @icon}
+        class="flex w-16 h-16 items-center justify-center rounded-full bg-bronze-100"
+      >
+        <img
+          :if={@image_url}
+          src={@image_url}
+          alt={@title || "card image"}
+          class="h-8 w-8 object-cover"
+        />
+        <.icon :if={!@image_url && @icon} name={@icon} class="h-8 w-8 text-bronze-500" />
       </div>
       <div class="flex flex-col items-center gap-3 text-center">
         <h3
           :if={@title}
-          class="font-amarante text-bronze-500 text-xl tracking-[1px] leading-[28px] uppercase text-center px-4"
+          class="font-amarante text-bronze-500 text-xl tracking-[1px] leading-7 uppercase text-center px-4"
         >
           {@title}
         </h3>
