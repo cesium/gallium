54d158c0eb9d170b154029ebafbf9eb4d4c6b144
Gabriel Faria <gabriel.ldfaria@gmail.com>
2026-03-16 14:01:14 +0000

    fix: colors

--- lib/gallium_web/components/buttons.ex
+++ lib/gallium_web/components/buttons.ex
@@ -6,8 +6,17 @@ defmodule GalliumWeb.Components.PrimaryButton do
   import GalliumWeb.CoreComponents
 
   attr :text, :string, doc: "Button text"
-  attr :bgcolor, :string, doc: "Background color"
-  attr :textcolor, :string, doc: "Text color"
+  attr :color, :atom, values: [:olive, :blue, :bronze, :light_muted], default: :olive, doc: "Background color"
+  attr :text_color, :atom, values: [:auto, :olive, :blue, :bronze], default: :auto, doc: "Text color"
+
+  @color_classes %{
+    olive: "bg-olive text-light-muted hover:bg-olive-700",
+    blue: "bg-blue text-light-muted hover:bg-blue-700",
+    bronze: "bg-bronze text-light-muted hover:bg-bronze-700",
+    light_muted: "bg-light-muted"
+  }
+  @text_colors %{auto: "text-blue", olive: "text-olive", blue: "text-blue", bronze: "text-bronze"}
+
   attr :icon, :string, default: nil, doc: "Icon to be displayed"
 
   attr :iconpos, :atom,
@@ -37,8 +46,7 @@ defmodule GalliumWeb.Components.PrimaryButton do
       class={[
         "flex w-full h-full items-center justify-center gap-2 px-4 py-2 rounded-md
          cursor-pointer hover:scale-95 transition-all duration-300",
-        @bgcolor,
-        @textcolor,
+        color_classes(@color, @text_color),
         @rest[:class]
       ]}
       {@rest}
@@ -53,4 +61,10 @@ defmodule GalliumWeb.Components.PrimaryButton do
     </button>
     """
   end
+
+  defp color_classes(:light_muted, color),
+    do: [@color_classes.light_muted, Map.fetch!(@text_colors, color)]
+
+  defp color_classes(color, _text_color),
+    do: [Map.fetch!(@color_classes, color)]
 end
