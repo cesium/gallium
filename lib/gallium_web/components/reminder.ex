1cc6155f5c36df49681feb140953616f0a5498fa
a111430 <a111430@alunos.uminho.pt>
2026-03-16 16:15:31 +0000

    fix: container width and height

--- lib/gallium_web/components/reminder.ex
+++ lib/gallium_web/components/reminder.ex
@@ -1,7 +1,5 @@
 defmodule GalliumWeb.Components.Reminder do
-  @moduledoc """
-  Reminder Component.
-  """
+  @moduledoc false
   use Phoenix.Component
 
   attr :title, :string, required: true, doc: "The title of the reminder"
@@ -9,7 +7,7 @@ defmodule GalliumWeb.Components.Reminder do
 
   def reminder(assigns) do
     ~H"""
-    <div class="flex flex-col gap-3 items-center justify-center rounded-lg border border-olive/10 bg-olive-200/30 w-[1216px] h-[138px]">
+    <div class="flex flex-col p-10 sm:px-0 gap-3 items-center justify-center text-center rounded-lg border border-olive/10 bg-olive-200/30 w-full h-fit">
       <h2 class="text-olive/80 font-amarante uppercase text-2xl tracking-wide">
         {@title}
       </h2>
