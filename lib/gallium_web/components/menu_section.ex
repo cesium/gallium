608c880bdd0056cb8b64864335c686bd95adab3e
PedroCoutinho <edr0c0de@protonmail.com>
2026-03-16 14:44:06 +0000

    feat: rework

--- lib/gallium_web/components/menu_section.ex
+++ lib/gallium_web/components/menu_section.ex
@@ -12,26 +12,21 @@ defmodule GalliumWeb.MenuSection do
 
   def menu_frame(assigns) do
     ~H"""
-    <div class="relative w-256 font-serif">
-      <img
-        src="/frames/frame 4.svg"
-        class="absolute inset-0 w-full h-full object-fill pointer-events-none"
-      />
-
-      <div class="relative flex flex-col items-center gap-6 p-20">
-        <h1 class="text-3xl tracking-[0.4em] text-[#7BAFC4] font-serif">{@title}</h1>
-
-        <%= for {section, index} <- Enum.with_index(@section) do %>
-          <%= if index > 0 do %>
-            <div class="w-48 h-px bg-[#7BAFC4] opacity-50"></div>
-          <% end %>
-          <div class="flex flex-col items-center gap-2 text-center">
-            <h2 class="text-sm tracking-[0.3em] text-[#7BAFC4] font-semibold">{section.title}</h2>
-            <div class="text-[10px] tracking-widest text-[#7BAFC4] max-w-xl leading-relaxed uppercase">
-              {render_slot(section)}
+    <div class="relative w-full h-auto p-6 bg-beige bg-[url(/frames/frame4.svg)] bg-contain bg-no-repeat bg-center">
+      <div class="relative flex flex-col items-center gap-8 p-12">
+        <h1 class="text-5xl  text-blue-500 font-amarante py-2">{@title}</h1>
+        <div class="">
+          <%= for {section, i} <- Enum.with_index(@section) do %>
+            <div class="flex flex-col items-center text-center p-4 gap-2">
+              <h2 class="text-md tracking-[0.3em] text-blue-500 font-amarante font-semibold uppercase">
+                {section.title}
+              </h2>
+              <div class="text-sm tracking-wide text-black font-cormorant  leading-relaxed text-wrap max-w-5xl uppercase">
+                {render_slot(section)}
+              </div>
             </div>
-          </div>
-        <% end %>
+          <% end %>
+        </div>
       </div>
     </div>
     """
