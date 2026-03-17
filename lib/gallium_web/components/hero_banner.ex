2ef46c874a8205e0b15d2c0bc782b60000ea8503
PedroCoutinho <edr0c0de@protonmail.com>
2026-03-17 16:06:09 +0000

    fix:line break

--- lib/gallium_web/components/hero_banner.ex
+++ lib/gallium_web/components/hero_banner.ex
@@ -6,24 +6,21 @@ defmodule GalliumWeb.HeroBanner do
 
   attr :title, :string, default: "MENU"
 
-  slot :section, required: true do
-    attr :title, :string, required: true
-  end
-
   def hero_banner(assigns) do
     ~H"""
-    <div class="bg-olive py-8 ">
-      <div class="relative w-full font-serif h-auto bg-[url(/frames/frame2.svg)] bg-contain bg-no-repeat bg-center">
+    <div class="bg-olive w-full px-8 sm:px-0">
+      <div class="relative w-full font-serif h-auto sm:bg-[url(/frames/frame2.svg)] bg-contain bg-no-repeat bg-center">
         <div class=" flex flex-col items-center justify-center text-center py-3">
           <div class="flex flex-col gap-3 p-12">
             <p class="flex items-center justify-center text-white font-bold text-md tracking-[.25em] opacity-50">
               •••••••••
             </p>
-            <h1 class="text-olive-50 font-amarante leading-tight tracking-wide text-5xl m-0">
+            <h1 class="text-olive-50 font-amarante leading-tight tracking-wide text-6xl m-0">
               JANTAR<br />DE GALA
             </h1>
             <p class="text-olive-50 font-cormorant font-bold text-xs tracking-[0.2em]">
-              PROGRAMA • VER MAIS >>>
+              <span class="block sm:inline">PROGRAMA •</span>
+              <span class="block sm:inline">VER MAIS &gt;&gt;&gt</span>
             </p>
           </div>
 
