7aeb443e05e8c0bf48c1f0685d3e0fb8b18fa64d
a111430 <a111430@alunos.uminho.pt>
2026-03-21 00:02:07 +0000

    fix: brand name and navbar responsiveness

--- lib/gallium_web/components/navbar.ex
+++ lib/gallium_web/components/navbar.ex
@@ -7,32 +7,36 @@ defmodule GalliumWeb.Components.Navbar do
   import GalliumWeb.Components.Button
 
   attr :ticket_url, :string, default: "", doc: "URL for purchasing tickets"
+  attr :landing_pages, :list, default: [], doc: "List of landing pages for the navbar navigation"
   attr :class, :any, default: nil, doc: "The navbar class to use over defaults"
 
   def navbar(assigns) do
     ~H"""
-    <header class={"w-full h-fit bg-beige text-blue-800/80 px-3 py-3 #{@class}"}>
-      <div class="max-w-7xl mx-auto flex items-center justify-between tracking-widest">
-        <div>
+    <header class={"w-full h-fit bg-beige text-blue-800/80 px-4 py-3 #{@class}"}>
+      <div class="max-w-7xl mx-auto flex items-center justify-between tracking-widest gap-4">
+        <div class="flex-1 min-w-max">
           <.link href="/">
-            <p class="font-amarante text-blue-500 text-2xl uppercase cursor-pointer">
-              galium
+            <p class="font-amarante text-blue-500 text-xl md:text-2xl uppercase cursor-pointer">
+              jantar de gala
             </p>
           </.link>
         </div>
 
-        <nav class="md:flex gap-10 uppercase font-cormorant font-bold text-xs">
-          <.link href="" class="hover:text-blue-500 transition-colors">evento</.link>
-          <.link href="" class="hover:text-blue-500 transition-colors">bilhetes</.link>
+        <nav class="flex flex-wrap gap-x-4 md:gap-x-10 justify-center uppercase font-cormorant font-bold text-[10px] md:text-xs">
+          <%= for page <- @landing_pages do %>
+            <.link navigate={page.url} class="hover:text-blue-500 transition-colors">
+              {page.name}
+            </.link>
+          <% end %>
         </nav>
 
-        <div>
+        <div class="flex-1 flex min-w-max justify-end">
           <.primary_button
             text="comprar bilhetes"
             link={@ticket_url}
             color={:blue}
             text_color={:auto}
-            class="bg-blue-500 text-beige! px-6 py-1 font-amarante font-bold uppercase"
+            class="bg-blue-500 text-beige! px-4 md:px-6 py-1 font-amarante font-bold uppercase text-[10px] md:text-xs"
           />
         </div>
       </div>
