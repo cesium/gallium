ee1ef221af4da602dfdbe2547c5d667dddab3f67
PedroCoutinho <edr0c0de@protonmail.com>
2026-04-01 00:11:16 +0100

    feat: add new frames

--- lib/gallium_web/components/menu_section.ex
+++ lib/gallium_web/components/menu_section.ex
@@ -3,6 +3,7 @@ defmodule GalliumWeb.MenuSection do
   This module holds the menu for the Jantar de Gala.
   """
   use Phoenix.Component
+  import GalliumWeb.Components.Frame
 
   attr :title, :string, default: "MENU"
 
@@ -14,11 +15,12 @@ defmodule GalliumWeb.MenuSection do
     ~H"""
     <div class="relative w-full h-auto p-6 bg-beige bg-[url(/frames/frame4.svg)] bg-contain bg-no-repeat bg-center">
       <div class="relative flex flex-col items-center gap-8 p-12">
-        <h1 class="text-5xl  text-blue-500 font-cormorant py-2">{@title}</h1>
+        <.frame style={:style1} mode={:bottom} color={:blue} />
+        <h1 class="text-6xl  text-blue-500 font-amarante tracking-wide  py-2">{@title}</h1>
         <div class="">
           <%= for {section, i} <- Enum.with_index(@section) do %>
             <div class="flex flex-col items-center text-center p-4 gap-2">
-              <h2 class="text-md tracking-[0.3em] text-blue-500 font-amarante font-semibold uppercase">
+              <h2 class="text-xl tracking-[0.3em] text-blue-500 font-amarante font-semibold uppercase">
                 {section.title}
               </h2>
               <div class="text-sm tracking-wide text-black font-cormorant  leading-relaxed text-wrap max-w-5xl uppercase">
@@ -27,6 +29,7 @@ defmodule GalliumWeb.MenuSection do
             </div>
           <% end %>
         </div>
+        <.frame style={:style1} mode={:top} color={:blue} />
       </div>
     </div>
     """
