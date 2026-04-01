4602053132935790a6f96e8ad427d504b4112309
PedroCoutinho <edr0c0de@protonmail.com>
2026-04-01 20:39:07 +0100

    fix: review suggestions

--- lib/gallium_web/components/menu_section.ex
+++ lib/gallium_web/components/menu_section.ex
@@ -1,4 +1,4 @@
-defmodule GalliumWeb.MenuSection do
+defmodule GalliumWeb.Components.MenuSection do
   @moduledoc """
   This module holds the menu for the Jantar de Gala.
   """
@@ -7,26 +7,34 @@ defmodule GalliumWeb.MenuSection do
 
   attr :title, :string, default: "MENU"
 
-  slot :section, required: true do
+  slot :section do
     attr :title, :string, required: true
   end
 
   def menu_frame(assigns) do
     ~H"""
-    <div class="relative w-full h-auto p-6 bg-beige bg-[url(/frames/frame4.svg)] bg-contain bg-no-repeat bg-center">
+    <div class="relative w-full h-auto p-6 bg-beige bg-contain bg-no-repeat bg-center">
       <div class="relative flex flex-col items-center gap-8 p-12">
         <.frame style={:style1} mode={:bottom} color={:blue} />
-        <h1 class="text-6xl  text-blue-500 font-amarante tracking-wide  py-2">{@title}</h1>
-        <div class="">
-          <%= for {section, i} <- Enum.with_index(@section) do %>
+        <h1 class="text-6xl text-blue-500 font-amarante tracking-wide py-2">{@title}</h1>
+        <div>
+          <%= if @section == [] do %>
             <div class="flex flex-col items-center text-center p-4 gap-2">
-              <h2 class="text-xl tracking-[0.3em] text-blue-500 font-amarante font-semibold uppercase">
-                {section.title}
-              </h2>
-              <div class="text-sm tracking-wide text-black font-cormorant  leading-relaxed text-wrap max-w-5xl uppercase">
-                {render_slot(section)}
-              </div>
+              <p class="text-sm tracking-wide text-black font-cormorant leading-relaxed text-wrap max-w-5xl uppercase">
+                Sem secções para mostrar.
+              </p>
             </div>
+          <% else %>
+            <%= for {section, _} <- Enum.with_index(@section) do %>
+              <div class="flex flex-col items-center text-center p-4 gap-2">
+                <h2 class="text-xl tracking-[0.3em] text-blue-500 font-amarante font-semibold uppercase">
+                  {section.title}
+                </h2>
+                <div class="text-sm tracking-wide text-gray-500 font-cormorant leading-relaxed text-wrap max-w-5xl uppercase">
+                  {render_slot(section)}
+                </div>
+              </div>
+            <% end %>
           <% end %>
         </div>
         <.frame style={:style1} mode={:top} color={:blue} />
