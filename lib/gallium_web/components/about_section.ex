70faadefb12d0fdec88782a201a0897ffd21067b
Brumag <a110274@alunos.uminho.pt>
2026-04-07 16:14:53 +0100

    feat: About Section Component (#38)
    Co-authored-by: Afonso Martins <145707571+AfonsoMartins26@users.noreply.github.com>

---
+++ lib/gallium_web/components/about_section.ex
@@ -0,0 +1,44 @@
+defmodule GalliumWeb.Components.AboutSection do
+  @moduledoc """
+  About section component
+  """
+  use GalliumWeb, :html
+  import GalliumWeb.Components.Frame
+
+  attr :class, :string, default: nil
+  attr :title, :string, required: true
+  attr :description, :string, required: true
+
+  slot :inner_block, required: false
+
+  def about_section(assigns) do
+    ~H"""
+    <section class={[
+      "bg-light-muted w-full h-screen py-24 px-6 flex flex-col items-center justify-center text-center",
+      @class
+    ]}>
+      <div class="mb-6 rotate-180 scale-130">
+        <.frame style={:style2} mode={:bottom} color={:olive} class="scale-75" />
+      </div>
+
+      <div class="max-w-3xl mx-auto">
+        <h2 class="text-olive font-amarante text-4xl sm:text-5xl md:text-6xl uppercase leading-tight mb-8 tracking-wide">
+          {raw(
+            @title
+            |> String.replace("\\n", "\n")
+            |> String.replace("\n", "<br/>")
+          )}
+        </h2>
+
+        <h4 class="text-bronze-950/60 font-cormorant text-lg md:text-2xl font-medium mx-auto leading-relaxed">
+          {@description}
+        </h4>
+      </div>
+
+      <div class="mt-6 scale-130">
+        <.frame style={:style2} mode={:bottom} color={:olive} class="scale-75" />
+      </div>
+    </section>
+    """
+  end
+end
