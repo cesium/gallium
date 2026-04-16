9c506e7733c91da9f6589140e8fa07f2a95ada55
a111430 <a111430@alunos.uminho.pt>
2026-04-16 19:11:24 +0100

    feat: landing page

--- lib/gallium_web/live/landing/index.ex
+++ lib/gallium_web/live/landing/index.ex
@@ -1,5 +1,9 @@
 defmodule GalliumWeb.LandingLive.Index do
-  use GalliumWeb, :app_view
+  use GalliumWeb, :landing_view
+
+  import GalliumWeb.Components.Hero
+  import GalliumWeb.Components.AboutSection
+  import GalliumWeb.Components.Forecast
 
   @impl true
   def mount(_params, _session, socket) do
